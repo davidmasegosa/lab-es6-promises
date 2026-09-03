fd1642c1ac6b02a25a72ac332c241c9cc54d5aea
David Masegosa <davidmasegosa@gmail.com>
2026-09-03 09:33:18 +0200

    Make the mashed potatoes with callbacks

--- javascript/index.js
+++ javascript/index.js
@@ -29,7 +29,31 @@
 */
 
 // Iteration 1 - using callbacks
-// ...
+getInstruction("mashedPotatoes", 0, (step0) => {
+  document.querySelector("#mashedPotatoes").innerHTML += `<li>${step0}</li>`;
+
+  getInstruction("mashedPotatoes", 1, (step1) => {
+    document.querySelector("#mashedPotatoes").innerHTML += `<li>${step1}</li>`;
+
+    getInstruction("mashedPotatoes", 2, (step2) => {
+      document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
+
+      getInstruction("mashedPotatoes", 3, (step3) => {
+        document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
+
+        getInstruction("mashedPotatoes", 4, (step4) => {
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>${step4}</li>`;
+          document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+          document.querySelector("#mashedPotatoesImg").removeAttribute("hidden");
+        }, (error) => console.log(error));
+
+      }, (error) => console.log(error));
+
+    }, (error) => console.log(error));
+
+  }, (error) => console.log(error));
+
+}, (error) => console.log(error));
 
 // Iteration 2 - using promises
 // ...
